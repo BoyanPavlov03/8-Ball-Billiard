146efa028719f89c13938f0a7f6ae51baf9cbcb2
jordanzz <jjordanzlatanov@gmail.com>
2021-07-12 21:53:40 +0300

    Cleaned code for cue stick

--- 8 Ball Pool/src/com/elsys/Cue.java
+++ 8 Ball Pool/src/com/elsys/Cue.java
@@ -22,45 +22,6 @@ public class Cue {
         return image;
     }
 
-    /*public static BufferedImage toBufferedImage(Image img)
-    {
-        if (img instanceof BufferedImage)
-        {
-            return (BufferedImage) img;
-        }
-
-        // Create a buffered image with transparency
-        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
-        // Draw the image on to the buffered image
-        Graphics2D bGr = bimage.createGraphics();
-        bGr.drawImage(img, 0, 0, null);
-        bGr.dispose();
-
-        // Return the buffered image
-        return bimage;
-    }*/
-
-    public BufferedImage toBufferedImage(Image img)
-    {
-        if (img instanceof BufferedImage)
-        {
-            return (BufferedImage) img;
-        }
-
-        // Create a buffered image with transparency
-        BufferedImage bimage = new BufferedImage(img.getWidth(null) + 200, img.getHeight(null) + 200, BufferedImage.TYPE_INT_ARGB);
-
-        // Draw the image on to the buffered image
-        Graphics2D bGr = bimage.createGraphics();
-        bGr.drawImage(img, 0, 0, null);
-        bGr.dispose();
-
-        // Return the buffered image
-        return bimage;
-    }
-
-
     void tick(WhiteBall whiteBall, Vector2D mousePos){
         Vector2D whiteBallPos = new Vector2D(whiteBall.position.x + 14, whiteBall.position.y + 14);
         double dist = whiteBallPos.distance(mousePos);
@@ -76,26 +37,11 @@ public class Cue {
 
     void render(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
-        BufferedImage bufferedImage = toBufferedImage(image);
 
         AffineTransform backup = g2d.getTransform();
-        //rx is the x coordinate for rotation, ry is the y coordinate for rotation, and angle
-        //is the angle to rotate the image. If you want to rotate around the center of an image,
-        //use the image's center x and y coordinates for rx and ry.
-        AffineTransform a = AffineTransform.getRotateInstance(Math.toRadians(0), position.x, position.y);
-        //Set our Graphics2D object to the transform
+        AffineTransform a = AffineTransform.getRotateInstance(Math.toRadians(200), position.x, position.y);
         g2d.setTransform(a);
-        //Draw our image like normal
         g2d.drawImage(image, (int) position.x, (int) position.y, null);
-        //Reset our graphics object so we can draw with it again.
         g2d.setTransform(backup);
-
-        /*double rotationRequired = Math.toRadians(0);
-        double locationX = bufferedImage.getWidth() / 2.0;
-        double locationY = bufferedImage.getHeight() / 2.0;
-        AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
-        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-        g2d.drawImage(op.filter(bufferedImage, null), (int)position.x, (int)position.y, null);*/
-
     }
 }
